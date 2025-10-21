e453bf8d7ff9f356969688c6cfb1b5a7ceb7ae87
mczekanski1 <38838492+mczek@users.noreply.github.com>
2025-10-21 16:39:42 -0400

    move dp5 weights to constant memory

--- src/firm3dpp/cuda_kernel.cu
+++ src/firm3dpp/cuda_kernel.cu
@@ -28,6 +28,15 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 // https://stackoverflow.com/questions/9116267/how-can-i-use-an-enumeration-as-a-template-parameter
 enum class RHS {GC_CartesianVacuum, GC_BoozerVacuum, GC_BoozerVacuumSAW};
 
+__constant__ double dp5_wgts[7][7] = {
+    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+    {1.0/5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+    {3.0 / 40.0, 9.0 / 40.0, 0.0, 0.0, 0.0, 0.0},
+    {44.0 / 45.0, -56.0 / 15.0, 32.0 / 9.0, 0.0, 0.0, 0.0, 0.0},
+    {19372.0 / 6561.0, -25360.0 / 2187.0, 64448.0 / 6561.0, -212.0 / 729.0, 0.0, 0.0, 0.0},
+    {9017.0 / 3168.0, -355.0 / 33.0, 46732.0 / 5247.0, 49.0 / 176.0,-5103.0 / 18656.0, 0.0, 0.0},
+    {35.0 / 384.0, 0.0, 500.0 / 1113.0, 125.0 / 192.0, -2187.0 / 6784.0, 11.0 / 84.0, 0.0}
+};
 
 // Particle Data Structure
 // This should eventually be removed
@@ -333,60 +342,13 @@ __device__ void build_state(double* x_temp, int deriv_id, bool* symmetry_exploit
                             double* r_shape, double* phi_shape, double* z_shape, double* state, double* derivs, double* dt,
                             double* rrange_arr, double* phirange_arr, double* zrange_arr){
 
-    double wgts[6] = {0.0}; 
     for (int i = 0; i < 4; i++) {
         x_temp[i*PARTICLES_PER_BLOCK + threadIdx.x] = state[i*PARTICLES_PER_BLOCK + threadIdx.x];
     }
 
-    switch(deriv_id){
-        case 0:
-            // wgts = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-            break;
-        case 1:
-            // wgts = {1.0/5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-            wgts[0] = 1.0/5.0;
-            break;
-        case 2:
-            // wgts = {3.0 / 40.0, 9.0 / 40.0, 0.0, 0.0, 0.0, 0.0};
-            wgts[0] = 3.0 / 40.0;
-            wgts[1] = 9.0 / 40.0;
-            break;
-        case 3:
-            // wgts = {44.0 / 45.0, -56.0 / 15.0, 32.0 / 9.0, 0.0, 0.0, 0.0, 0.0};
-            wgts[0] = 44.0 / 45.0;
-            wgts[1] = -56.0 / 15.0;
-            wgts[2] = 32.0 / 9.0;
-            break;
-        case 4:
-            // wgts = {19372.0 / 6561.0, -25360.0 / 2187.0, 64448.0 / 6561.0, -212.0 / 729.0, 0.0, 0.0, 0.0};
-            wgts[0] = 19372.0 / 6561.0;
-            wgts[1] = -25360.0 / 2187.0;
-            wgts[2] = 64448.0 / 6561.0;
-            wgts[3] = -212.0 / 729.0;
-            break;
-        case 5:
-            // wgts = {9017.0 / 3168.0, -355.0 / 33.0, 46732.0 / 5247.0, 49.0 / 176.0,-5103.0 / 18656.0, 0.0, 0.0};
-            wgts[0] = 9017.0 / 3168.0;
-            wgts[1] = -355.0 / 33.0;
-            wgts[2] = 46732.0 / 5247.0;
-            wgts[3] = 49.0 / 176.0;
-            wgts[4] = -5103.0 / 18656.0;
-            break;
-        case 6:
-            // wgts = {35.0 / 384.0, 0.0, 500.0 / 1113.0, 125.0 / 192.0, -2187.0 / 6784.0, 11.0 / 84.0, 0.0};
-            wgts[0] = 35.0 / 384.0;
-            wgts[2] = 500.0 / 1113.0;
-            wgts[3] = 125.0 / 192.0; 
-            wgts[4] = -2187.0 / 6784.0;
-            wgts[5] = 11.0 / 84.0;
-            break;
-        default:
-            break;
-    }
-
-    for (int j=0; j<6; ++j){
+    for (int j=0; j<deriv_id; ++j){
         for(int i=0; i<4; ++i){
-            x_temp[i*PARTICLES_PER_BLOCK + threadIdx.x] += dt[threadIdx.x] * wgts[j] * derivs[(6*j+i)*PARTICLES_PER_BLOCK + threadIdx.x];
+            x_temp[i*PARTICLES_PER_BLOCK + threadIdx.x] += dt[threadIdx.x] * dp5_wgts[deriv_id][j] * derivs[(6*j+i)*PARTICLES_PER_BLOCK + threadIdx.x];
         }
     } 
 
