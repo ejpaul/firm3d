e37ad5e65982302792f26031b4516a70cc7004c8
Alexandra Lachmann <alachman@login24.chn.perlmutter.nersc.gov>
2026-05-05 05:04:54 -0700

    removed peta poinc

--- examples/WBA_particles/sample_slurm.sh
+++ examples/WBA_particles/sample_slurm.sh
@@ -6,5 +6,5 @@
 #SBATCH --account=m4680 # Change to your account number
 
 module load python cray-hdf5/1.14.3.1 cray-netcdf/4.9.0.13
-conda activate firm3d # Change to the name of your environment
+conda activate equilibrium_map_firm3d # Change to the name of your environment
 srun -n 128 -c 1 python -u return_DA.py
