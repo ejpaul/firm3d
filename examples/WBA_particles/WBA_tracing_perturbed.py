1c5aa544959b7b4b885316931c0e2274061dea44
Alexandra Lachmann <alexandralachmann@Alexandras-MacBook-Pro-1477.local>
2026-07-08 15:26:41 -0400

    forgot one last edit

--- examples/WBA_particles/WBA_tracing_perturbed.py
+++ examples/WBA_particles/WBA_tracing_perturbed.py
@@ -81,7 +81,7 @@ vpar_init = initialize_velocity_uniform(vpar0, nParticles, comm=comm, seed=0)
 
 field.set_points(tracing_points)
 modB = field.modB()[:, 0]
-mus_per_mass = (Ekin - 0.5 * vpar_init**2) / (modB * mass)
+mus_per_mass = (0.5 * vpar0**2 - 0.5 * vpar_init**2) / (modB)
 
 object_WBA = WBAPerturbedParticles(
     saw,
